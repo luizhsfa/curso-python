baa09b5cf16e0ad7b202de7db192a8f54404cfbd
LUIZ HENRIQUE <luiz.ferrera1994@gmail.com>
2025-04-04 21:32:58 -0300

    criado o ex007
    Feito meu exercício ex007 onde tive que criar um código para determinar a media d nota do aluno.

---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digiite a segunda nota do aluno: '))
+m = (n1 + n2) / 2
+print('A média entre {} e {} é igual a {}.'.format(n1, n2, m))
